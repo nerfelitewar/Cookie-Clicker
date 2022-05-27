6fda07467fb4ffdbcf481edbae7a27b39122816e
nerfelitewar <80751691+nerfelitewar@users.noreply.github.com>
2022-05-28 02:24:50 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+//HACKS MAKES COOKIE BITTER :/ 
+function changeImage() {
+    var Sound = document.getElementById('music_play');
+    if (Sound.src.match("Images/music_speaker.png")) {
+        Sound.src = "Images/music_speaker.png";
+        Sound.src = "Images/mute_music_speaker.png";
+    }
+    else {
+        Sound.src = "Images/music_speaker.png";
+    }
+
+}  
+
+var cookiecount =0;
+
+function add(){
+    cookiecount= cookiecount+1
+    document.getElementById('high-score').value = cookiecount;
+}
+
+const reloadtButton = document.querySelector("#reload");
+
+function reload() {
+    reload = location.reload();
+}
+
+reloadButton.addEventListener("click", reload, false);
+
+
+
+function playclip() {
+
+     {
+      document.sound.src = "click.mp3";
+     }
+    }
+  
+    
